c30c9cb7a9e0f0881807acd544f7207e0aeb9a86
ADMIN <1@gmail.com>
2025-02-25 19:49:49 +0300

    business logic in "domain" layer

--- app/src/main/java/alex/android/lab/domain/repositories/ProductsRepository.kt
+++ app/src/main/java/alex/android/lab/domain/repositories/ProductsRepository.kt
@@ -4,7 +4,15 @@ import alex.android.lab.domain.entities.Product
 
 interface ProductsRepository {
 
-    fun getProducts(): List<Product>
+    suspend fun syncProductsWithApi()
 
-    fun getProductById(guid: String): Product
+    suspend fun getProducts(): List<Product>
+
+    suspend fun getProductById(guid: String): Product
+
+    suspend fun updateProductViewCount(guid: String, viewCount: Int)
+
+    suspend fun updateProductInCartStatus(guid: String, isInCart: Boolean)
+
+    suspend fun updateProductFavoriteStatus(guid: String, isFavorite: Boolean)
 }
